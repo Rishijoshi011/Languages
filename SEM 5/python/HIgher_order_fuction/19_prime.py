d090e5fee53d8ffaeec614ab2393b4c256ad2fb7
Rishi <81611604+Rishijoshi011@users.noreply.github.com>
2024-07-19 11:05:04 +0530

    Update 19_prime.py

--- SEM 5/python/HIgher_order_fuction/19_prime.py
+++ SEM 5/python/HIgher_order_fuction/19_prime.py
@@ -1,1 +1,18 @@
-list = [3, 4, 6, 9, 11]
+list1 = [3, 4, 6, 9, 11]
+
+def isPrime(num):
+    if num <= 1:
+        return "NP"
+    elif num == 2: 
+        return "P"         # * 2 is only even prime number
+    elif num % 2 == 0:
+        return "NP"        
+    else:
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return "NP"
+        return "P"
+
+result = map(lambda x: isPrime(x), list1)
+
+print(list(result))
